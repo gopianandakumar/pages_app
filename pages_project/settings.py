e5ab42a00de19e5892709e192f1cd72a3575836e
gopianandakumar <gopianandakumar@gmail.com>
2022-08-13 13:59:34 +0530

    New updates for Heroku deployment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y&6p7bg325itmkb%he7*fyjl=%rw5g_lfl0!68j29^srgozfwv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
